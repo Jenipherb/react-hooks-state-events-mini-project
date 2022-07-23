32e899708aaa612f8615c76fc65d2a74c353e715
Jenipher Belloso <jenipherbelloso@gmail.com>
2022-07-22 20:33:48 -0600

    src

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,9 +1,24 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({cat, selectedCategory, onSelectedCategory}) {
+  // console.log(category)
+  // console.log(selectedCategory)
+  // console.log(onSelectedCategory)
+  const catButton = cat.map((category) =>{
+    const className = category === selectedCategory ? "selected" : null;
+    return(
+      <button
+        key={category}
+        className={className}
+        onClick={() => onSelectedCategory(category)}>
+          {category}
+        </button>
+    )
+  })
   return (
     <div className="categories">
       <h5>Category filters</h5>
+      {catButton}
       {/* render <button> elements for each category here */}
     </div>
   );
