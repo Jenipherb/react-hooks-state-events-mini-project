e974e7e12af9f297c73db9450b48445bc9bc8b9f
Jenipher Belloso <jenipherbelloso@gmail.com>
2022-07-22 20:33:28 -0600

    commit tree 8eaf57f9b647bf5a4bf5c894efe0bf834df15c56

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewTaskForm() {
+function NewTaskForm({cat, onTaskFormSubmit}) {
+  const [text, setText] = useState("")
+  const [category, setCategory] = useState("Code")
+
+  function handleSubmit(e){
+    e.preventDefault()
+    onTaskFormSubmit({text, category})
+    setText("")
+    setCategory("Code")
+  }
   return (
-    <form className="new-task-form">
+    <form className="new-task-form" onSubmit={handleSubmit}>
       <label>
         Details
-        <input type="text" name="text" />
+        <input type="text" name="text" 
+          value={text}
+          onChange={(e) => setText(e.target.value)}/>
       </label>
       <label>
         Category
-        <select name="category">
+        <select name="category" value={category}
+          onChange={(e) => setCategory(e.target.value)}>
+          {cat.map((cat) => (
+            <option key={cat}>{cat}</option>
+          ))}  
           {/* render <option> elements for each category here */}
         </select>
       </label>
